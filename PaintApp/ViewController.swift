209ef7c0675749c2fd8d89d62d53c9ac888b2f05
unawares <unawares15@gmail.com>
2019-09-28 15:00:00 +0600

    Initial Commit

---
+++ PaintApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaintApp
+//
+//  Created by Theodore Teddy on 9/28/19.
+//  Copyright © 2019 Theodore Teddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PaintAppTests/PaintAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  PaintAppTests.swift
+//  PaintAppTests
+//
+//  Created by Theodore Teddy on 9/28/19.
+//  Copyright © 2019 Theodore Teddy. All rights reserved.
+//
+
+import XCTest
+@testable import PaintApp
+
+class PaintAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
